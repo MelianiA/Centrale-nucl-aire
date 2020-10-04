d7ba0234a9865eb54b7e2151ca94fdaf97dcfc1b
meliania <49156284+meliania@users.noreply.github.com>
2020-10-04 13:45:39 +0200

    Utilisation de Event et EventArgs.

--- Centrale nucléaire/Program.cs
+++ Centrale nucléaire/Program.cs
@@ -1,43 +1,65 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 
+ 
 namespace Centrale_nucléaire
 {
+    public delegate void declencherdel(PompeEventArgs e);
     class Centrale_nucleaire
     {
-        List<IPompe> pompes = new List<IPompe>() 
-        {
-            new Pompe_electrique(),
-            new Pompe_hydrauliques(), 
-            new Pompe_hydrauliques() 
-        };
+        public ArrayList pompes = null;
+        public event declencherdel listdel;
+        public int temperature = 3000;
 
+        public Centrale_nucleaire()
+        {
+            pompes = new ArrayList()
+                {
+                    new Pompe_electrique(),
+                    new Pompe_hydrauliques(),
+                    new Pompe_hydrauliques() ,
+                    new Pompe_manuelle()
+                };
+       
+            listdel += ((Pompe_electrique)pompes[0]).declencher;
+            listdel += ((Pompe_hydrauliques)pompes[1]).declencher;
+            listdel += ((Pompe_hydrauliques)pompes[2]).declencher;
+            listdel += ((Pompe_manuelle)pompes[3]).declencher;
+        }
+        PompeEventArgs tmp = new PompeEventArgs() {temperature = 5000, pression= 155};
         public void seRefroidir()
         {
-            foreach (var item in pompes)
-            {
-                item.declencher();
-            }
-          
+            listdel(tmp);
         }
     }
-    interface IPompe
+
+    public class PompeEventArgs : EventArgs
     {
-        public void declencher();
+        public int temperature { get; set; }
+        public int pression { get; set; }
     }
-    class Pompe_electrique :IPompe
+    class Pompe_electrique  
     {
-        public void declencher()
+        public void declencher(PompeEventArgs ev)
         {
-            Console.WriteLine("Pompe électrique lancée");
+            Console.WriteLine("Pompe hydraulique lancée, [Température: " + ev.temperature + ", Pression = "+ev.pression+" bar]");
         }
     }
-    class Pompe_hydrauliques : IPompe
+    class Pompe_hydrauliques
     {
-        public void declencher()
+        public void declencher(PompeEventArgs ev)
         {
-            Console.WriteLine("Pompe hydraulique lancée");
+            Console.WriteLine("Pompe hydraulique lancée, [Température: " + ev.temperature + ", Pression = " + ev.pression + " bar]");
+        }
+    }
+    class Pompe_manuelle  
+    {
+        public void declencher(PompeEventArgs ev)
+        {
+            Console.WriteLine("Pompe hydraulique lancée, [Température: " + ev.temperature + ", Pression = " + ev.pression + " bar]");
         }
     }
     class Program
@@ -47,7 +69,6 @@ namespace Centrale_nucléaire
             Centrale_nucleaire ctn = new Centrale_nucleaire();
             ctn.seRefroidir();
             Console.ReadKey();
-
         }
     }
 }
